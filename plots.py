35f0041c33d37ba94067cb2ba00ad40b763ea996
даня <pslpslpslpsl@yandex.ru>
2019-12-21 14:47:47 +0300

    changed algorithm to iterate through vector rather than indexate and to pass iterators as parametres

--- plots.py
+++ plots.py
@@ -9,19 +9,20 @@ for filename in glob.glob('*.txt'):
    results[i] = numpy.loadtxt(filename)
    i += 1
    
-
+   
 
 axis1 = numpy.arange(0, len(results[0]), 1)
 fig = matplotlib.pyplot.figure()
 
 
+
 for i in range(len(results)):
    matplotlib.pyplot.subplot(1, 1, i+1)
-   matplotlib.pyplot.scatter(axis1, results[i])
+   matplotlib.pyplot.plot(axis1, results[i])
    matplotlib.pyplot.title('Moment of order ' + str(i))
-    
+
 frame1 = matplotlib.pyplot.gca()
-frame1.get_yaxis().set_visible(False)
+frame1.get_yaxis().set_visible(True)
 matplotlib.pyplot.show()
     
     
