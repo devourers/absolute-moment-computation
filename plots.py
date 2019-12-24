5d50ed32de35bd60eea72084b9347a8179656c91
danya <pslpslpslpsl@yandex.ru>
2019-12-24 16:11:24 +0300

    working version on release debugger and plots for it@

--- plots.py
+++ plots.py
@@ -6,20 +6,26 @@ import glob
 results = [[] for f in glob.glob('*.txt')]
 i = 0
 for filename in glob.glob('*.txt'):
-   results[i] = numpy.loadtxt(filename)
+   results = numpy.loadtxt(filename)
    i += 1
    
+print(results)
    
 
-axis1 = numpy.arange(0, len(results[0]), 1)
+axis1 = numpy.arange(0, len(results), 1)
 fig = matplotlib.pyplot.figure()
 
 
 
-for i in range(len(results)):
-   matplotlib.pyplot.subplot(1, 1, i+1)
-   matplotlib.pyplot.plot(axis1, results[i])
-   matplotlib.pyplot.title('Moment of order ' + str(i))
+matplotlib.pyplot.plot(axis1, results[:,0])
+matplotlib.pyplot.title('Moment of order 10 via 2 pass algorithm')
+matplotlib.pyplot.xlabel('Number of elements in A')
+matplotlib.pyplot.ylabel('Time to compute')
+
+#for i in range(len(results)):
+#   matplotlib.pyplot.subplot(1, 1, i+1)
+#   matplotlib.pyplot.plot(axis1, results[i])
+
 
 frame1 = matplotlib.pyplot.gca()
 frame1.get_yaxis().set_visible(True)
