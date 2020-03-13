4c32c9f4aa44bdf4c3e5f578459f60d30b2bad54
danya <pslpslpslpsl@yandex.ru>
2020-03-13 21:01:24 +0300

    setup for moment compuataion + overhaul of inner struct structure (sic)

--- treap.cpp
+++ treap.cpp
@@ -1,45 +1,83 @@
 ﻿#include <iostream>;
+#include <random>;
 
 struct node {
-	int key, priority;
-	node * l, *r;
+	double key, sum, priority, sqauredSum;
+	int nodes;
+	node* l, * r;
 	node() { }
-	node(int key, int priority) : key(key), priority(priority), l(NULL), r(NULL) { }
+	node(double key, double priority) : key(key), priority(priority), l(nullptr), r(nullptr), sum(key), sqauredSum(key* key), nodes(1) { }
 };
-typedef node * tnode;
+typedef node* tnode;
 
-void split(tnode t, int key, tnode & l, tnode & r) {
+void updateSum(tnode t)
+{
+	if (t == nullptr) {
+
+	}
+	else if (t->l == nullptr && t->r == nullptr) {
+		t->sum = t->key;
+		t->sqauredSum = t->key * t->key;
+		t->nodes = 1;
+	}
+	else if (t->l == nullptr)
+	{
+		t->sum = t->key + t->r->sum;
+		t->sqauredSum = t->key * t->key + t->r->sqauredSum;
+		t->nodes = 1 + t->r->nodes;
+	}
+	else if (t->r == nullptr) {
+		t->sum = t->key + t->l->sum;
+		t->sqauredSum = t->key * t->key + t->l->sqauredSum;
+		t->nodes = 1 + t->l->nodes;
+	}
+	else
+	{
+		t->sum = t->key + t->l->sum + t->r->sum;
+		t->sqauredSum = t->key * t->key + t->r->sqauredSum + t->l->sqauredSum;
+		t->nodes = 1 + t->r->nodes + t->l->nodes;
+	}
+
+}
+
+void split(tnode t, double key, tnode& l, tnode& r) {
 	if (!t)
 		l = r = NULL;
 	else if (key < t->key)
 		split(t->l, key, l, t->l), r = t;
 	else
 		split(t->r, key, t->r, r), l = t;
+	updateSum(l);
+	updateSum(r);
+
 }
 
-void insert(tnode & t, tnode it) {
+void insert(tnode& t, tnode it) {
 	if (!t)
 		t = it;
 	else if (it->priority > t->priority)
 		split(t, it->key, it->l, it->r), t = it;
 	else
 		insert(it->key < t->key ? t->l : t->r, it);
+	updateSum(t);
 }
 
-void merge(tnode & t, tnode l, tnode r) {
+void merge(tnode& t, tnode l, tnode r) {
 	if (!l || !r)
 		t = l ? l : r;
 	else if (l->priority > r->priority)
 		merge(l->r, l->r, r), t = l;
 	else
 		merge(r->l, l, r->l), t = r;
+	updateSum(t);
 }
 
-void erase(tnode & t, int key) {
+void erase(tnode& t, double key) {
 	if (t->key == key)
 		merge(t, t->l, t->r);
 	else
 		erase(key < t->key ? t->l : t->r, key);
+	updateSum(t);
 }
 
 tnode unite(tnode l, tnode r) {
@@ -50,4 +88,59 @@ tnode unite(tnode l, tnode r) {
 	l->l = unite(l->l, lt);
 	l->r = unite(l->r, rt);
 	return l;
+}
+
+double countMoment(tnode& t) {
+	node* lesser;
+	node* greater;
+	double divKey = t->sum / t->nodes;
+	split(t, divKey, lesser, greater);
+	return 0;
+}
+
+
+int main()
+{
+	double currKey;
+	double currPrior;
+	double realSum = 0;
+	double squRealSum = 0;
+	int a = 1000;
+	currKey = rand() % a;
+	currKey /= a;
+	realSum += currKey;
+	squRealSum += currKey * currKey;
+	currPrior = rand() % 100;
+	currPrior /= 100;
+	node* tree = new node(currKey, currPrior);
+	std::cout << currKey << std::endl;
+	for (int i = 0; i < a; i++) {
+		currKey = rand() % a;
+		currKey /= a;
+		realSum += currKey;
+		squRealSum += currKey * currKey;
+		currPrior = rand() % (i + 100);
+		currPrior /= (i + 100);
+		node* curr = new node(currKey, currPrior);
+		std::cout << currKey << std::endl;
+		insert(tree, curr);
+	}
+	//node* ae = new node(100, 1);
+	//node* b = new node(150, 2);
+	//node* c = new node(125, 3);
+	//node* d = new node(75, 0.1);
+	//node* e = new node(200, 2.5);
+	//node* t = unite(ae, b);
+	//insert(t, c);
+	//insert(t, d);
+	//insert(t, e);
+	std::cout << "key " << tree->key << std::endl;
+	//std::cout << tree->l->key << std::endl;
+	//std::cout << tree->l->l->key << std::endl;
+	//std::cout << tree->r->key << std::endl;
+	std::cout << "alg " << tree->sum << std::endl;
+	std::cout << "real " << realSum << std::endl;
+	std::cout << "alg squr " << tree->sqauredSum << std::endl;
+	std::cout << "real " << squRealSum << std::endl;
+	std::cout << "nodes " << tree->nodes << std::endl;
 }
