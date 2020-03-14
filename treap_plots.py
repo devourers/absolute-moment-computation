3387aa57e583c0b81dd54cd446b15747469564c6
danya <pslpslpslpsl@yandex.ru>
2020-03-14 16:21:02 +0300

    added plot file and plot pic

--- treap_plots.py
+++ treap_plots.py
@@ -4,15 +4,18 @@ import os
 import glob
 
 results_treap = numpy.loadtxt("treap_results.txt")
+results_2pass = numpy.loadtxt("2pass_results.txt")
 
 axis1 = numpy.arange(0, len(results_treap), 1)
 
-print(results_treap)
+#print(results_treap)
+print(len(results_2pass))
 
 fig = matplotlib.pyplot.figure()
 
 matplotlib.pyplot.plot(axis1, results_treap[:,0])
-matplotlib.pyplot.title('Moment of order 2 via treap algorithm')
+matplotlib.pyplot.plot(axis1, results_2pass[:,0])
+matplotlib.pyplot.title('Moment of order 2')
 matplotlib.pyplot.xlabel('Number of elements in A')
 matplotlib.pyplot.ylabel('Time to compute')
 
